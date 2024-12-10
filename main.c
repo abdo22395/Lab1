6bd015b6fc572c710588edbffd9478e68825318a
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-10 18:01:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,22 +14,22 @@ int main() {
     usleep(2000000); // Wait for 2 seconds
 
     // Shift a byte of data to the right
-    unsigned char data = 0b11111111; // Start with all bits set
-    printf("Shifting data to the right:\n");
-    for (int i = 0; i < 8; i++) {
-        my_shift(data, data_pin, clk_pin, latch_pin, false); // Shift right
-        usleep(1000000); // Wait for 1 second
-        data >>= 1; // Shift right
-    }
+unsigned char data = 0b11111111; // Start with all bits set
+printf("Shifting data to the right:\n");
+for (int i = 0; i < 8; i++) {
+    my_shift(data, data_pin, clk_pin, latch_pin, false); // Shift right
+    usleep(1000000); // Wait for 1 second
+    data >>= 1; // Shift right
+}
 
-    // Shift a byte of data to the left
-    data = 0b00000001; // Start with the least significant bit set
-    printf("Shifting data to the left:\n");
-    for (int i = 0; i < 8; i++) {
-        my_shift(data, data_pin, clk_pin, latch_pin, true); // Shift left
-        usleep(1000000); // Wait for 1 second
-        data <<= 1; // Shift left
-    }
+// Shift a byte of data to the left
+data = 0b00000001; // Start with the least significant bit set
+printf("Shifting data to the left:\n");
+for (int i = 0; i < 8; i++) {
+    my_shift(data, data_pin, clk_pin, latch_pin, true); // Shift left
+    usleep(1000000); // Wait for 1 second
+    data <<= 1; // Shift left
+}
 
     // Clear all LEDs
     printf("Clearing all LEDs...\n");
