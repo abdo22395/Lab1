2e8a112b1478e276384e79060df761b6cd8da2af
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-10 17:41:52 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include "my_74hc595_driver.h"
 #include "my_8bit_lib.h"
 #include "my_drivertest.h"
-#include <stdio.h>
 
 int main() {
     // Define GPIO pin numbers
     const char *data_pin = "16"; // Data pin
-    const char *latch_pin = "20"; // Latch pin (not used in my_shift function, but can be used if needed)
-    const char *clk_pin = "21"; // Clock pin
+    const char *latch_pin = "20"; // Latch pin
+    const char *clk_pin = "21 "; // Clock pin
 
     // Turn on all LEDs
     printf("Turning on all LEDs...\n");
@@ -18,7 +17,7 @@ int main() {
     unsigned char data = 0b11111111; // Start with all bits set
     printf("Shifting data to the right:\n");
     for (int i = 0; i < 8; i++) {
-        my_shift(data, data_pin, clk_pin, false); // Shift right
+        my_shift(data, data_pin, clk_pin, latch_pin, false); // Shift right
         usleep(1000000); // Wait for 1 second
         data >>= 1; // Shift right
     }
@@ -27,7 +26,7 @@ int main() {
     data = 0b00000001; // Start with the least significant bit set
     printf("Shifting data to the left:\n");
     for (int i = 0; i < 8; i++) {
-        my_shift(data, data_pin, clk_pin, true); // Shift left
+        my_shift(data, data_pin, clk_pin, latch_pin, true); // Shift left
         usleep(1000000); // Wait for 1 second
         data <<= 1; // Shift left
     }
