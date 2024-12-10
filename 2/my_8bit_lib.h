25d3a5eda81441d64dd98af2463ff258ad347886
abdo22395 <41839202+abdo22395@users.noreply.github.com>
2024-12-10 19:36:01 +0100

    Delete 2 directory

--- 2/my_8bit_lib.h
+++
@@ -1,9 +0,0 @@
-#ifndef MY_8BIT_LIB_H
-#define MY_8BIT_LIB_H
-
-#include <stdint.h>
-
-// Funktion för att flippa en specifik bit i en bitsträng av okänd storlek
-void my_flip(uint8_t *data, int bit);
-
-#endif // MY_8BIT_LIB_H
